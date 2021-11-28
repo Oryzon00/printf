d02d2f52f5adb038cee8ec97c2f378cf2d04f737
Adrian JUNG <ajung@e1r3p10.42.fr>
2021-11-28 19:56:38 +0100

    push auto

--- srcs/ft_pustr_pf.c
+++ srcs/ft_pustr_pf.c
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.h                                           :+:      :+:    :+:   */
+/*   ft_pustr.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/28 16:28:31 by ajung             #+#    #+#             */
-/*   Updated: 2021/11/28 19:56:14 by ajung            ###   ########.fr       */
+/*   Created: 2021/11/28 18:38:11 by ajung             #+#    #+#             */
+/*   Updated: 2021/11/28 18:43:12 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRINTF_H
-# define PRINTF_H
+#include "../printf.h"
 
-# include "libft/libft.h"
-# include <stdarg.h>
+int	ft_putstr_pf(char *s)
+{
+	int	i;
+	int	len_str;
 
-void	what_var(char c, int *result, va_list arg_list);
-int		ft_printf(const char *str, ...);
-int		ft_putnbr_pf(int n);
-int		ft_putstr_pf(char *s);
-#endif
+	len_str = 0;
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		write(1, s[i], 1);
+		i++;
+		len_str++;
+	}
+	return (len_str);
+}
